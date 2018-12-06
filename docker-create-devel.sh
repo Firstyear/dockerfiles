c6edb58568e03cf9ca8cbe58c940fbcd2356fb46
William Brown <william@blackhats.net.au>
2018-12-06 20:01:18 +1000

    Add opensuse

--- docker-create-devel.sh
+++ docker-create-devel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "distro version"
-echo $1 $2
-sudo /usr/bin/docker create -i -t --privileged=true -h ldapkdc.example.com -v /home/$USER/development:/home/$USER/development $1_devel:$2
+# echo "distro version"
+# echo $1 $2
+sudo /usr/bin/docker create -i -t --privileged=true -h ldapkdc.example.com -v /home/$USER/development:/home/$USER/development opensuse_devel:latest
+# sudo /usr/bin/docker create -i -t --privileged=true -h ldapkdc.example.com -v /home/$USER/development:/home/$USER/development $1_devel:$2
 
--- docker-remove-old-inst.sh
+++ docker-remove-old-inst.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for image in `sudo docker ps -a | grep -E '.*(weeks|days).*' | awk '{ print $1; }' `; do
+for image in `sudo docker ps -a | grep -E '.*(weeks|days|months).*' | awk '{ print $1; }' `; do
 echo $image
 sudo docker rm $image
 done
